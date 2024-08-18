af7723c43c929012ce11a6547b49787e7d92d4a4
Ankit Mod <ankit300mod@gmail.com>
2024-08-18 18:52:13 +0530

    data pushed

--- src/BackGround.jsx
+++ src/BackGround.jsx
@@ -5,14 +5,8 @@ const BackGround = () => {
   return (
     <div className='flex justify-center items-center h-full w-full'>
       <video 
-        src={Video} 
-        className='w-full' 
-        style={{ height: '500px', objectFit: 'cover' }} 
-        autoPlay 
-        loop 
-        
-      ></video>
-    </div>
+        src={Video} className='w-full' style={{ height: '500px', objectFit: 'cover' }} autoPlay loop muted ></video>
+    </div> 
   );
 };
 
